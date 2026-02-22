bd854da417f5f9a90f2a81be7a4a1e15a9a6f0cf
Bhargav <vyasb1503@gmail.com>
2026-02-22 21:59:46 +0530

    first commit

---
+++ src/test/java/com/bhargav/errormonitor/roottrace/RoottraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhargav.errormonitor.roottrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoottraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
