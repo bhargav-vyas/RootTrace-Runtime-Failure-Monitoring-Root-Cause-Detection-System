1a9db0a712be7f6bac5dbf4fd75c87acb754e326
Bhargav <vyasb1503@gmail.com>
2026-03-03 23:01:28 +0530

    implement the  controller Demo controller

--- src/test/java/com/bhargav/roottrace/controller/DemoController.java
+++ src/test/java/com/bhargav/roottrace/controller/DemoController.java
@@ -1,22 +1,22 @@
-package com.bhargav.roottrace.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class DemoController {
-    @GetMapping("/")
-    public String home(){
-        return "RootTrace is running";
-    }
-    @GetMapping("/null-error")
-    public String nullError(){
-        String value = null;
-        return  value.toString();
-    }
-    @GetMapping("/divide-error")
-    public int divideError(){
-         return 10/0;
-    }
-
-
-}
+//package com.bhargav.roottrace.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//@RestController
+//public class DemoController {
+//    @GetMapping("/")
+//    public String home(){
+//        return "RootTrace is running";
+//    }
+//    @GetMapping("/null-error")
+//    public String nullError(){
+//        String value = null;
+//        return  value.toString();
+//    }
+//    @GetMapping("/divide-error")
+//    public int divideError(){
+//         return 10/0;
+//    }
+//
+//
+//}
