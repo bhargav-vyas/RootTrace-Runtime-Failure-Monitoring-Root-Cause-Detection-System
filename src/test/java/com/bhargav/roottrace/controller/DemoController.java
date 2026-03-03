3ef5a79348b42eadf87384fabc166b54d6f085ae
Bhargav <vyasb1503@gmail.com>
2026-03-03 22:39:32 +0530

    implement the  controller Demo controller

--- src/test/java/com/bhargav/roottrace/controller/DemoController.java
+++ src/test/java/com/bhargav/roottrace/controller/DemoController.java
@@ -1,19 +1,19 @@
-package com.bhargav.roottrace.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class DemoController {
-    @GetMapping("/")
-    public String home(){
-        return "RootTrace is running";
-    }
-    @GetMapping("/null-error")
-    public String nullError(){
-        String value = null;
-        return  value.toString();
-    }
-
-
-
-}
+//package com.bhargav.roottrace.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//@RestController
+//public class DemoController {
+//    @GetMapping("/")
+//    public String home(){
+//        return "RootTrace is running";
+//    }
+//    @GetMapping("/null-error")
+//    public String nullError(){
+//        String value = null;
+//        return  value.toString();
+//    }
+//
+//
+//
+//}
