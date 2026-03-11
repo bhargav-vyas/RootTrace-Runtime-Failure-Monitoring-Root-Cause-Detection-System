7bb5e264ac0c7d34e8934383dade2c5ec4d58ac3
Bhargav <vyasb1503@gmail.com>
2026-03-11 17:35:58 +0530

    implementing the  exception handler thing

--- src/test/java/com/bhargav/roottrace/exceptioion/GlobalExceptionHandler.java
+++ src/test/java/com/bhargav/roottrace/exceptioion/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.bhargav.roottrace.exceptioion;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(Exception ex) {
+        System.out.println("===== ROOTTRACE ERROR CAPTURED =====");
+        System.out.println("Exception Type: " + ex.getClass().getName());
+        System.out.println("Message: " + ex.getMessage());
+        System.out.println("Time: " + LocalDateTime.now());
+
+    }
 }
