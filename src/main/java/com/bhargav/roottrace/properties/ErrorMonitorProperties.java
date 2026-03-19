d368d40fc457f5fb26ff9ef29687aef6494eb8ad
Bhargav <vyasb1503@gmail.com>
2026-03-19 21:10:28 +0530

    implement ther erroemonitorproperties  and  api and enable configuration to connmect any sdk change the   name of the roottrace

--- src/main/java/com/bhargav/roottrace/properties/ErrorMonitorProperties.java
+++ src/main/java/com/bhargav/roottrace/properties/ErrorMonitorProperties.java
@@ -1,35 +1,35 @@
-//package com.bhargav.roottrace.properties;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//@ConfigurationProperties(prefix = "roottrace")
-//public class ErrorMonitorProperties {
-//
-//
-//    private boolean enable = true;
-//    private String apikey;
-//    private String  serverUrl;
-//
-//    public boolean isEnable() {
-//        return enable;
-//    }
-//
-//    public void setEnable(boolean enable) {
-//        this.enable = enable;
-//    }
-//
-//    public String getApikey() {
-//        return apikey;
-//    }
-//
-//    public void setApikey(String apikey) {
-//        this.apikey = apikey;
-//    }
-//
-//    public String getServerUrl() {
-//        return serverUrl;
-//    }
-//
-//    public void setServerUrl(String serverUrl) {
-//        this.serverUrl = serverUrl;
-//    }
-//}
+package com.bhargav.roottrace.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+@ConfigurationProperties(prefix = "roottrace")
+public class ErrorMonitorProperties {
+
+
+    private boolean enable = true;
+    private String apikey;
+    private String  serverUrl;
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+}
